dfb88629cc15fc0aadbf4cb21290076a89ab1359
Nathan Szurek <nathan.szurek@yahoo.com>
2020-07-15 22:06:54 -0500

    Altered schema and seeds files

--- db/seeds.sql
+++ db/seeds.sql
@@ -10,3 +10,12 @@ INSERT INTO role (title, salary, department_id) VALUES ('Software Engineer', 120
 INSERT INTO role (title, salary, department_id) VALUES ('Accountant', 125000, 3);
 INSERT INTO role (title, salary, department_id) VALUES ('Legal Team Lead', 250000, 4);
 INSERT INTO role (title, salary, department_id) VALUES ('Lawyer', 190000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id) VALUES ('Tom', 'Ferald', 1);
+INSERT INTO employee (first_name, last_name, role_id) VALUES ('Alfred', 'Smith', 2);
+INSERT INTO employee (first_name, last_name, role_id) VALUES ('George', 'Carlos', 2);
+INSERT INTO employee (first_name, last_name, role_id) VALUES ('Rick', 'Svannah', 3);
+INSERT INTO employee (first_name, last_name, role_id) VALUES ('Ethyl', 'Denver', 4);
+INSERT INTO employee (first_name, last_name, role_id) VALUES ('Roberta', 'Clark', 5);
+INSERT INTO employee (first_name, last_name, role_id) VALUES ('Samuel', 'Jackson', 6);
+INSERT INTO employee (first_name, last_name, role_id) VALUES ('Carter', 'Brahm', 7);
