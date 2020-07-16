b235cf418da5c4122219c0f5dcc38ce4f225c993
Nathan Szurek <nathan.szurek@yahoo.com>
2020-07-16 17:34:04 -0500

    Recreated seeds to accept all information

--- db/seeds.sql
+++ db/seeds.sql
@@ -11,11 +11,11 @@ INSERT INTO role (title, salary, department_id) VALUES ('Accountant', 125000, 3)
 INSERT INTO role (title, salary, department_id) VALUES ('Legal Team Lead', 250000, 4);
 INSERT INTO role (title, salary, department_id) VALUES ('Lawyer', 190000, 4);
 
-INSERT INTO employee (first_name, last_name, role_id) VALUES ('Tom', 'Ferald', 1);
-INSERT INTO employee (first_name, last_name, role_id) VALUES ('Alfred', 'Smith', 2);
-INSERT INTO employee (first_name, last_name, role_id) VALUES ('George', 'Carlos', 2);
-INSERT INTO employee (first_name, last_name, role_id) VALUES ('Rick', 'Svannah', 3);
-INSERT INTO employee (first_name, last_name, role_id) VALUES ('Ethyl', 'Denver', 4);
-INSERT INTO employee (first_name, last_name, role_id) VALUES ('Roberta', 'Clark', 5);
-INSERT INTO employee (first_name, last_name, role_id) VALUES ('Samuel', 'Jackson', 6);
-INSERT INTO employee (first_name, last_name, role_id) VALUES ('Carter', 'Brahm', 7);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Tom', 'Ferald', 1, NULL);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Alfred', 'Smith', 2, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('George', 'Carlos', 2, NULL);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Rick', 'Svannah', 3, NULL);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Ethyl', 'Denver', 4, NULL);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Roberta', 'Clark', 5, NULL);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Samuel', 'Jackson', 6, NULL);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Carter', 'Brahm', 7, NULL);
