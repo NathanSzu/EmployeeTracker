1398a7d57b894edb9b0618e1c287d2a1c7897345
Nathan Szurek <nathan.szurek@yahoo.com>
2020-07-16 18:02:17 -0500

    Created connection.js to link my db to my server

---
+++ config/connection.js
@@ -0,0 +1,27 @@
+// Set up MySQL connection.
+const mysql = require("mysql");
+// Set up util to allow promisify.
+const util = require('util');
+
+// Connection details.
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "Sokka1992!",
+  database: "employeeTracker_db"
+});
+
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+connection.query = util.promisify(connection.query);
+
+// Export connection for our ORM to use.
+module.exports = connection;
